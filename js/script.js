5470c4a641744cd0dae38d3b297b6a0d2e894fca
JeanPPG <jppenaranda@espe.edu.ec>
2024-02-06 18:54:53 -0500

    Inicio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function openTasksMenu() {
+    alert("Función para abrir el menú de Tareas");
+}
+
+function openProjectsMenu() {
+    alert("Función para abrir el menú de Proyectos");
+}
+
+function openReportsMenu() {
+    alert("Función para abrir el menú de Reportes");
+}
